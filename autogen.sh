f362c6f19d55c093d4ef413933258c187a7b130a
rpahu32323 <rpahu32323@gmail.com>
2017-03-15 20:02:40 -0400

    Initial program files.

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+# bootstrap the project
+
+# make the bin directory
+mkdir bin
+
+# get the project ready
+autoreconf -i
---
+++ src/Main.cpp
@@ -0,0 +1,23 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Feb 19, 2017
+ *      Author: rpahu
+ */
+
+// c++ library includes
+#include <iostream>
+
+// main function
+int main( int argc, char **argv )
+{
+	// hello world
+	std::cout<<"Hello World"<<std::endl;
+
+	// return finished
+	return( 0 );
+}
+
+
+
+
